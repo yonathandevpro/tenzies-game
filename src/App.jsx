9967497a6279597c91469388a8cc8c673d02c11b
Yonathan Berhanu <yonathan.berhanu17@gmail.com>
2024-04-19 14:55:50 +0300

    Applied CSS styles for Dice components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from 'react';
+import Die from './Die';
 
 function App() {
   
@@ -7,11 +7,23 @@ function App() {
     <main>
         <div className="container">
             <div className="game-floor">
-                hahah
+                <div className="die-grid">
+                    <Die val={1} />
+                    <Die val={2} />
+                    <Die val={3} />
+                    <Die val={2} />
+                    <Die val={5} />
+                    <Die val={6} />
+                    <Die val={1} />
+                    <Die val={4} />
+                    <Die val={3} />
+                    <Die val={2} />
+                </div>
+                 
             </div>
         </div>
     </main>
-  )
+  );
 }
 
 export default App
---
+++ src/Die.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Die({val}) {
+    return (
+        <div className="die-nums">
+                <h1>{val}</h1>
+        </div>
+    );
+}
