7713c5e9dc83e197de076e701bd5cc6d36962e21
Yonathan Berhanu <yonathan.berhanu17@gmail.com>
2024-04-19 16:20:40 +0300

    Changed state from array of nums to array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,23 @@ import { nanoid } from 'nanoid'
 function App() {
 
   const allNewDice = () => {
-    const randomNums = [];
+    const newDice = []
     for (let i = 0; i < 10; i++) {
-         randomNums.push(Math.ceil(Math.random() * 6)); 
+        newDice.push({value: Math.ceil(Math.random() * 6), isHeld: false});
     }
-    return randomNums;
-}
+    return newDice;
+  }
 
-    const [ diceNums, setDiceNums ] = useState(allNewDice);
-    const diceComponents = diceNums.map(diceNum => <Die key={nanoid()} val={diceNum}/>);
-  
+  const [ diceNums, setDiceNums ] = useState(allNewDice);
+
+  const diceComponents = diceNums.map(diceNum => <Die key={nanoid()} val={diceNum.value}/>);
+    
+   
+
      function handleRollClick() {
           setDiceNums(allNewDice());
      }
-     
+
   return (
     <main>
         <div className="container">
@@ -28,7 +31,7 @@ function App() {
                 <div className="die-grid">
                     {diceComponents}
                 </div>
-                <button onClick={handleRollClick} class="roll-btn">Roll</button>  
+                <button onClick={handleRollClick} className="roll-btn">Roll</button>  
             </div>
         </div>
     </main>
